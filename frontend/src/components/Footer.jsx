3220ba1afaae18c53999aa9267e9739f1fd7bb67
Ajita369 <162597493+Ajita369@users.noreply.github.com>
2025-01-25 18:32:21 +0530

    Added new project

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container">
+        <div className="banner">
+          <div className="left">SavorSpot</div>
+          <div className="right">
+            <p>Hotel in Aerocity,New Delhi</p>
+            <p>Open: 05:00 PM - 12:00 AM</p>
+          </div>
+        </div>
+        <div className="banner">
+          <div className="right">
+            <p>All Rights Reserved.</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
